c89778213778acac9c22a97a4205c9ee361c68a9
sobaniw <sobani.darshi@gmail.com>
2025-10-10 01:57:51 +0530

    feat/adding db name home

--- DramaLand/includes/db_connect.php
+++ DramaLand/includes/db_connect.php
@@ -2,7 +2,7 @@
 // includes/db_connect.php
 require_once 'config.php';
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, dramaland_db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
