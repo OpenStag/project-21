57ce7e973adaeba5d422c67958c35a24addf68bc
sobaniw <sobani.darshi@gmail.com>
2025-12-08 02:22:42 +0530

    feat/adding navbar.php href../

--- DramaLand/includes/navbar.php
+++ DramaLand/includes/navbar.php
@@ -7,7 +7,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
 </head>
 <body>
     <nav class="navbar">
